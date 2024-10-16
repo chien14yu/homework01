4c1384cf34ede99c457bc708039e97839b429c37
chengyi <0524eric@gmail.com>
2024-10-16 13:42:44 +0800

    Initial Commit

---
+++ homework01/homework01App.swift
@@ -0,0 +1,17 @@
+//
+//  homework01App.swift
+//  homework01
+//
+//  Created by user05 on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct homework01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ homework01Tests/homework01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  homework01Tests.swift
+//  homework01Tests
+//
+//  Created by user05 on 2024/10/16.
+//
+
+import Testing
+@testable import homework01
+
+struct homework01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
